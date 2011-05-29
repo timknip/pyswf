169143c1bcc78ddabeba175d05ab9a8e48ae4e93
timk <tim@floorplanner.com>
2011-05-29 21:40:57 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(name='pyswf',
+      version='1.0',
+      description='SWF Parsing Utilities',
+      author='Tim Knip',
+      author_email='tim@floorplanner.com',
+      url='https://github.com/timknip/pyswf',
+      packages=find_packages()
+      )
