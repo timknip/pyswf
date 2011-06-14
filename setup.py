76c6db6578ecc112b0cafea7edf3b05ca3a0af47
timk <tim@floorplanner.com>
2011-06-14 19:42:53 +0200

    adding dependencies to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup, find_packages
 setup(name='pyswf',
-      version='1.0',
+      version='1.1',
       description='SWF Parsing Utilities',
       author='Tim Knip',
       author_email='tim@floorplanner.com',
       url='https://github.com/timknip/pyswf',
+      install_requires = ["lxml==2.3", "PIL==1.1.7"],
       packages=find_packages()
       )
