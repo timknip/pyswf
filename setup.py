fa59e394b4b24c04f69818f619caca03b6c366ac
timk <tim@floorplanner.com>
2012-01-25 17:25:57 +0100

    bumping to version 1.2

--- setup.py
+++ setup.py
@@ -1,10 +1,27 @@
+import os
 from setuptools import setup, find_packages
+
+# Utility function to read the README file.  
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    
 setup(name='pyswf',
-      version='1.1',
+      version='1.2',
       description='SWF Parsing Utilities',
+      long_description=read('README.md'),
+      keywords = "swf parser parsing decompile utilities",
       author='Tim Knip',
       author_email='tim@floorplanner.com',
       url='https://github.com/timknip/pyswf',
       install_requires = ["lxml==2.3", "PIL==1.1.7"],
-      packages=find_packages()
+      packages=find_packages(),
+      license = "MIT",
+      classifiers=[
+          "Development Status :: 4 - Beta",
+          "Topic :: Utilities",
+          "License :: OSI Approved :: MIT License",
+      ],
       )
