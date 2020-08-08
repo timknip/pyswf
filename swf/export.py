d0078048128a7034533cdd9ada81e437b00187b4
Chris Midgley <midgleyc@gmail.com>
2020-08-08 17:01:27 +0100

    Some strings need to be bytestrings.

--- swf/export.py
+++ swf/export.py
@@ -421,7 +421,7 @@ class BaseExporter(object):
                 image_data = image.getdata()
                 image_data_len = len(image_data)
                 if num_alpha == image_data_len:
-                    buff = ""
+                    buff = b""
                     for i in range(0, num_alpha):
                         alpha = ord(tag.bitmapAlphaData.read(1))
                         rgb = list(image_data[i])
@@ -1100,10 +1100,10 @@ class SVGBounds(object):
         self._matrix = self._calc_combined_matrix()
 
 def _encode_jpeg(data):
-    return "data:image/jpeg;base64," + base64.encodestring(data)[:-1]
+    return b"data:image/jpeg;base64," + base64.encodestring(data)[:-1]
 
 def _encode_png(data):
-    return "data:image/png;base64," + base64.encodestring(data)[:-1]
+    return b"data:image/png;base64," + base64.encodestring(data)[:-1]
 
 def _swf_matrix_to_matrix(swf_matrix=None, need_scale=False, need_translate=True, need_rotation=False, unit_div=20.0):
 
